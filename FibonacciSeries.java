733ad3e67870b59ee78441c83d4f021436171006
UPOMA16 <upoma2201@gmail.com>
2026-05-23 19:26:51 +0600

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 12; 
+        int first = 0;
+        int second = 1;
+
+        System.out.print("Fibonacci series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        System.out.println();
+    }
+}
