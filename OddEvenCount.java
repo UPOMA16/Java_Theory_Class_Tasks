114ec8f503d70de5fb3e5c92eb882f87b4611b83
UPOMA16 <upoma2201@gmail.com>
2026-05-23 19:25:04 +0600

    Add files via upload

---
+++ OddEvenCount.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class OddEvenCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of even numbers: " + evenCount);
+        System.out.println("Number of odd numbers: " + oddCount);
+
+    
+    }
+}
